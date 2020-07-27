38ab1335ca9e86a75bd741d7c6db014804439f13
lli108 <liang.a.li@intel.com>
2020-07-27 16:41:08 +0800

    crop objects and save as image file

--- detect.py
+++ detect.py
@@ -8,10 +8,10 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
     "sofa", "train", "tvmonitor"]
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
-threshold = 0.5
+threshold = 0.8
 prototxt = 'E:/Data/model/mobilenet-ssd/MobileNetSSD_deploy.prototxt.txt'
 model = 'E:/Data/model/mobilenet-ssd/MobileNetSSD_deploy.caffemodel'
-srcvideo = 'test/cat2.jpg'
+srcvideo = 'test/dog.264'
 
 cap = cv2.VideoCapture(srcvideo)
 if not cap.isOpened():
@@ -20,19 +20,22 @@ if not cap.isOpened():
 
 net = cv2.dnn.readNetFromCaffe(prototxt, model)
 
+frame_count, obj_count = 0, 0
+
 while True:
     ret, frame = cap.read()
     if ret == False:
         break;
     (srch, srcw, _) = frame.shape
-    w2, h2 = (400, int(srch * (400/srcw)))
-    frame2 = cv2.resize(frame, (w2, h2), cv2.INTER_AREA)
+    scale = srcw/1000.0
+    w2, h2 = (1000, int(srch/scale))
+    frame2 = frame.copy() #cv2.resize(frame, (w2, h2), cv2.INTER_AREA)
     (h, w) = frame2.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(frame2, (300, 300)), 0.007843, (300, 300), 127.5)
 
     net.setInput(blob)
     detections = net.forward()
-
+    obj_count = 0
     for i in np.arange(0, detections.shape[2]):
         confidence = detections[0, 0, i, 2]
         if confidence > threshold:
@@ -40,9 +43,16 @@ while True:
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
             (startX, startY, endX, endY) = box.astype("int")
             label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+            # draw mask
             cv2.rectangle(frame2, (startX, startY), (endX, endY), COLORS[idx], 2)
             y = startY - 15 if startY - 15 > 15 else startY + 15
             cv2.putText(frame2, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+            # crop image and save to file
+            crop_img = frame[startY:endY, startX:endX]
+            crop_name = 'frame' + str(frame_count) + '_' + 'obj' + str(obj_count) + '.png'
+            cv2.imwrite(crop_name, crop_img)
+            obj_count += 1
+    frame_count += 1
     cv2.imshow("Frame", frame2)
 
     key = cv2.waitKey(0)
